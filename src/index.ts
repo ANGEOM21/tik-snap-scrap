d0e8c9c0fa30ea05087ca83a9e20cd34391a9534
ANGEOM21 <idrisanjounghan313@gmail.com>
2025-03-15 13:04:17 +0700

    update snaptik-crap

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 5000;
 const APP_URL = process.env.APP_URL || "http://localhost";
 // const __dirname = path.resolve();
 
-app.use(express.json(), cors());
+app.use(cors());
+app.use(express.json());
 
 // app.get("/", (req, res) => {
 // 	res.json({	
@@ -20,7 +21,7 @@ app.use(express.json(), cors());
 // 	});
 // });
 
-app.post("/api", async (req, res) => {
+app.post("/api/tik-tok", async (req, res) => {
 	try {
 		const { url } = req.body;
 		const result = await processUrl(url);
